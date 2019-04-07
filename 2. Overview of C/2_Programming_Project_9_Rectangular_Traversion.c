3941e0bc47b1d12831bf1e67bee2edda95fdcdfa
davelorino <davelorino@gmail.com>
2019-04-07 13:11:18 +1000

    Programming Project 9 - Rectangular Traversion

--- 2. Overview of C/2_Programming_Project_9_Rectangular_Traversion.c
+++ 2. Overview of C/2_Programming_Project_9_Rectangular_Traversion.c
@@ -1,2 +1,25 @@
+
+/* Write a program that takes the length of a rectangular yard and the length and width of a rectangular house situated in the yard. 
+ * Your program should compute the time required to cut the grass at the rate of two square feet per second.
+ */
+
 #include <stdio.h>
 
+int main(){
+  int length_of_yard, width_of_yard, time_to_cut_grass, square_feet_per_second, square_feet_of_yard;
+  
+  square_feet_per_second = 2;
+  
+  printf("Enter the length of the yard in feet> \n");
+  scanf("%d", &length_of_yard);
+  
+  printf("Enter the width of the yard in feet> \n");
+  scanf("%d", &width_of_yard);
+  
+  square_feet_of_yard = length_of_yard * width_of_yard;
+  
+  time_to_cut_grass = square_feet_of_yard / square_feet_per_second;
+  
+  printf("It waill take %d seconds to cut the grass in a yard of %d square feet\n", time_to_cut_grass, square_feet_of_yard);
+  return 0;
+}
