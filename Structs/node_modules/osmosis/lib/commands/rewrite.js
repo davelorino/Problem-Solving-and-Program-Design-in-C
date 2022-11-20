84687a33e9b9f36d15b338c6977edef0b25e2bfe
Davide Lorino <Davide@Davides-MacBook-Pro-2.local>
2022-11-20 21:38:57 +1100

    deleted some junk

--- Structs/node_modules/osmosis/lib/commands/rewrite.js
+++
@@ -1,5 +0,0 @@
-module.exports.rewrite = function (context, data, next, done) {
-    console.error('DEPRECATED. Use .find(selector).get(callback) instead.');
-    next(context, data);
-    done();
-};
